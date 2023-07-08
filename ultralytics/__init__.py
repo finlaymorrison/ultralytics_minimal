12f1885888044c15d2293ccabaa734a3031e373b
Finlay Morrison <morrison.fin02@gmail.com>
2023-07-08 09:46:18 +0100

    but as it turns out

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -2,9 +2,8 @@
 
 __version__ = '8.0.126'
 
-from ultralytics.hub import start
 from ultralytics.yolo.engine.model import YOLO
 from ultralytics.yolo.utils.checks import check_yolo as checks
 from ultralytics.yolo.utils.downloads import download
 
-__all__ = '__version__', 'YOLO', 'checks', 'start', 'download'  # allow simpler import
+__all__ = '__version__', 'YOLO', 'checks', 'download'  # allow simpler import
